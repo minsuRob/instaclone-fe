d645d6082327210b6d841a67c5b2bc6cb75777b5
minsu.lee <minsu.rob@gmail.com>
2024-07-28 00:25:30 +0900

    15.6 : 회원가입 메인 디자인 통일

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,12 +1,56 @@
 import React from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
+import AuthButton from "../components/auth/AuthButton";
+import AuthLayout from "../components/auth/AuthLayout";
+import { TextInput } from "react-native";
 
 const Container = styled.View`
   flex: 1;
-  background-color: black;
 `;
 
 export default function CreateAccount() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <AuthLayout>
+        <TextInput
+          placeholder="First Name"
+          placeholderTextColor="gray"
+          returnKeyType="next"
+          style={{ backgroundColor: "white", width: "100%" }}
+        />
+        <TextInput
+          placeholder="Last Name"
+          placeholderTextColor="gray"
+          returnKeyType="next"
+          style={{ backgroundColor: "white", width: "100%" }}
+        />
+        <TextInput
+          placeholder="Username"
+          placeholderTextColor="gray"
+          returnKeyType="next"
+          style={{ backgroundColor: "white", width: "100%" }}
+        />
+        <TextInput
+          placeholder="Email"
+          placeholderTextColor="gray"
+          keyboardType="email-address"
+          returnKeyType="next"
+          style={{ backgroundColor: "white", width: "100%" }}
+        />
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="gray"
+          secureTextEntry
+          returnKeyType="done"
+          style={{ backgroundColor: "white", width: "100%" }}
+        />
+        <AuthButton
+          text="Create Account"
+          disabled={true}
+          onPress={() => null}
+        />
+      </AuthLayout>
+    </Container>
+  );
 }
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -3,33 +3,8 @@ import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { colors } from "../colors";
 
-const Container = styled.View`
-  flex: 1;
-  background-color: black;
-  align-items: center;
-  justify-content: center;
-  padding: 0px 40px;
-`;
-
-const Logo = styled.Image`
-  max-width: 50%;
-  height: 100px;
-`;
-
-const CreateAccount = styled.TouchableOpacity`
-  border-color: ${colors.blue};
-  padding: 13px 10px;
-  margin-top: 20px;
-  width: 100%;
-  border-radius: 3px;
-  /* opacity: ${(props) => (props.disabled ? "0.5" : "1")}; */
-  /* opacity: ${(props) => (props.disabled ? "0.5" : "1")}; */
-`;
-const CreateAccountText = styled.Text`
-  color: white;
-  font-weight: 600;
-  text-align: center;
-`;
+import AuthButton from "../components/auth/AuthButton";
+import AuthLayout from "../components/auth/AuthLayout";
 
 const LoginLink = styled.Text`
   color: ${colors.blue};
@@ -41,14 +16,15 @@ export default function Welcome({ navigation }) {
   const goToCreateAccount = () => navigation.navigate("CreateAccount");
   const goToLogIn = () => navigation.navigate("LogIn");
   return (
-    <Container>
-      <Logo resizeMode="cover" source={require("../assets/logo.png")} />
-      <CreateAccount disabled={false} onPress={goToCreateAccount}>
-        <CreateAccountText>Create New Account</CreateAccountText>
-      </CreateAccount>
+    <AuthLayout>
+      <AuthButton
+        text="Crate New Account"
+        disabled={false}
+        onPress={goToCreateAccount}
+      />
       <TouchableOpacity onPress={goToLogIn}>
-        <LoginLink>Log in</LoginLink>
+        <LoginLink>Log In</LoginLink>
       </TouchableOpacity>
-    </Container>
+    </AuthLayout>
   );
 }
