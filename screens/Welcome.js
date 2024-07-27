ff8089e85d2a24401e7537715091540866d75ee8
minsu.lee <minsu.rob@gmail.com>
2024-07-27 21:31:17 +0900

    15.1 tab 이동

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function Welcome() {
+export default function Welcome({ navigation }) {
   return (
     <View>
       <Text>Welcome</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <Text>Go to Create Account</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("LogIn")}>
+        <Text>Go to Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
