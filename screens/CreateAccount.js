e78368f7e11f414f0ac4c73728ccc24f6fcfd4be
minsu.lee <minsu.rob@gmail.com>
2024-07-28 00:41:51 +0900

    15.7 일단 커밋

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -3,8 +3,7 @@ import { Text, View } from "react-native";
 import styled from "styled-components/native";
 import AuthButton from "../components/auth/AuthButton";
 import AuthLayout from "../components/auth/AuthLayout";
-import { TextInput } from "react-native";
-
+import { KeyboardAvoidingView, Platform, TextInput } from "react-native";
 const Container = styled.View`
   flex: 1;
 `;
@@ -26,52 +25,58 @@ export default function CreateAccount() {
   return (
     <Container>
       <AuthLayout>
-        <TextInput
-          placeholder="First Name"
-          placeholderTextColor="gray"
-          returnKeyType="next"
-          style={{ backgroundColor: "white", width: "100%" }}
-          onSubmitEditing={() => onNext(lastNameRef)}
-        />
-        <TextInput
-          ref={lastNameRef}
-          placeholder="Last Name"
-          placeholderTextColor="gray"
-          returnKeyType="next"
-          style={{ backgroundColor: "white", width: "100%" }}
-          onSubmitEditing={() => onNext(usernameRef)}
-        />
-        <TextInput
-          ref={usernameRef}
-          placeholder="Username"
-          placeholderTextColor="gray"
-          returnKeyType="next"
-          style={{ backgroundColor: "white", width: "100%" }}
-          onSubmitEditing={() => onNext(emailRef)}
-        />
-        <TextInput
-          ref={emailRef}
-          placeholder="Email"
-          placeholderTextColor="gray"
-          keyboardType="email-address"
-          returnKeyType="next"
-          style={{ backgroundColor: "white", width: "100%" }}
-          onSubmitEditing={() => onNext(passwordRef)}
-        />
-        <TextInput
-          ref={passwordRef}
-          placeholder="Password"
-          placeholderTextColor="gray"
-          secureTextEntry
-          returnKeyType="done"
-          style={{ backgroundColor: "white", width: "100%" }}
-          onSubmitEditing={() => onNext(onDone)}
-        />
-        <AuthButton
-          text="Create Account"
-          disabled={true}
-          onPress={() => null}
-        />
+        <KeyboardAvoidingView
+          style={{ width: "100%" }}
+          behavior="padding"
+          keyboardVerticalOffset={Platform.OS === "ios" ? 30 : 0}
+        >
+          <TextInput
+            placeholder="First Name"
+            placeholderTextColor="gray"
+            returnKeyType="next"
+            style={{ backgroundColor: "white", width: "100%" }}
+            onSubmitEditing={() => onNext(lastNameRef)}
+          />
+          <TextInput
+            ref={lastNameRef}
+            placeholder="Last Name"
+            placeholderTextColor="gray"
+            returnKeyType="next"
+            style={{ backgroundColor: "white", width: "100%" }}
+            onSubmitEditing={() => onNext(usernameRef)}
+          />
+          <TextInput
+            ref={usernameRef}
+            placeholder="Username"
+            placeholderTextColor="gray"
+            returnKeyType="next"
+            style={{ backgroundColor: "white", width: "100%" }}
+            onSubmitEditing={() => onNext(emailRef)}
+          />
+          <TextInput
+            ref={emailRef}
+            placeholder="Email"
+            placeholderTextColor="gray"
+            keyboardType="email-address"
+            returnKeyType="next"
+            style={{ backgroundColor: "white", width: "100%" }}
+            onSubmitEditing={() => onNext(passwordRef)}
+          />
+          <TextInput
+            ref={passwordRef}
+            placeholder="Password"
+            placeholderTextColor="gray"
+            secureTextEntry
+            returnKeyType="done"
+            style={{ backgroundColor: "white", width: "100%" }}
+            onSubmitEditing={() => onNext(onDone)}
+          />
+          <AuthButton
+            text="Create Account"
+            disabled={true}
+            onPress={() => null}
+          />
+        </KeyboardAvoidingView>
       </AuthLayout>
     </Container>
   );
