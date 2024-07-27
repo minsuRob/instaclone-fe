d29b7393cb8c2862f187fbf143aa2f95a343b364
minsu.lee <minsu.rob@gmail.com>
2024-07-28 00:34:48 +0900

    15.7 ux 측면 모바일 엔터 개선

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
 import AuthButton from "../components/auth/AuthButton";
@@ -9,6 +9,19 @@ const Container = styled.View`
   flex: 1;
 `;
 
+const lastNameRef = useRef();
+const usernameRef = useRef();
+const emailRef = useRef();
+const passwordRef = useRef();
+
+const onNext = (nextOne) => {
+  nextOne?.current?.focus();
+};
+
+const onDone = () => {
+  alert("done!");
+};
+
 export default function CreateAccount() {
   return (
     <Container>
@@ -18,32 +31,41 @@ export default function CreateAccount() {
           placeholderTextColor="gray"
           returnKeyType="next"
           style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(lastNameRef)}
         />
         <TextInput
+          ref={lastNameRef}
           placeholder="Last Name"
           placeholderTextColor="gray"
           returnKeyType="next"
           style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(usernameRef)}
         />
         <TextInput
+          ref={usernameRef}
           placeholder="Username"
           placeholderTextColor="gray"
           returnKeyType="next"
           style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(emailRef)}
         />
         <TextInput
+          ref={emailRef}
           placeholder="Email"
           placeholderTextColor="gray"
           keyboardType="email-address"
           returnKeyType="next"
           style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(passwordRef)}
         />
         <TextInput
+          ref={passwordRef}
           placeholder="Password"
           placeholderTextColor="gray"
           secureTextEntry
           returnKeyType="done"
           style={{ backgroundColor: "white", width: "100%" }}
+          onSubmitEditing={() => onNext(onDone)}
         />
         <AuthButton
           text="Create Account"
