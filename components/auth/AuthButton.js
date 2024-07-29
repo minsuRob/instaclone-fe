b09eb6a5a96c2160359f8dd8fcae8fd02a2fb24c
minsu.lee <minsu.rob@gmail.com>
2024-07-30 01:32:10 +0900

    15.12 : 버튼 로딩 모습 변경

--- components/auth/AuthButton.js
+++ components/auth/AuthButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components/native";
 import { colors } from "../../colors";
+import { ActivityIndicator } from "react-native-web";
 
 const Button = styled.TouchableOpacity`
   background-color: ${colors.blue};
@@ -16,10 +17,10 @@ const ButtonText = styled.Text`
   text-align: center;
 `;
 
-export default function AuthButton({ onPress, disabled, text }) {
+export default function AuthButton({ onPress, disabled, text, loading }) {
   return (
     <Button disabled={disabled} onPress={onPress}>
-      <ButtonText>{text}</ButtonText>
+      {loading ? <ActivityIndicator /> : <ButtonText>{text}</ButtonText>}
     </Button>
   );
 }
