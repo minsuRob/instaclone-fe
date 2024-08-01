0be76a82b566cae812b60b52bf995ff3348ae257
minsu.lee <minsu.rob@gmail.com>
2024-08-01 17:34:59 +0900

    16.5 : Feed 데이터 로드

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, View, ActivityIndicator, FlatList } from "react-native";
 import { gql, useQuery } from "@apollo/client";
 import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
+
+import {} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import ScreenLayout from "../components/ScreenLayout";
 
 const FEED_QUERY = gql`
   query seeFeed {
@@ -22,22 +26,24 @@ const FEED_QUERY = gql`
   ${PHOTO_FRAGMENT}
   ${COMMENT_FRAGMENT}
 `;
-export default function Feed({ navigation }) {
-  const { data } = useQuery(FEED_QUERY);
+export default function Feed({}) {
+  const { data, loading } = useQuery(FEED_QUERY);
   console.log(data);
+  const renderPhoto = ({ item: photo }) => {
+    return (
+      <View>
+        <Text>{photo.caption}</Text>
+      </View>
+    );
+  };
 
   return (
-    <View
-      style={{
-        backgroundColor: "black",
-        alignItems: "center",
-        justifyContent: "center",
-        flex: 1,
-      }}
-    >
-      <TouchableOpacity onPress={() => navigation.navigate("camera")}>
-        <Text style={{ color: "white" }}>Photo</Text>
-      </TouchableOpacity>
-    </View>
+    <ScreenLayout>
+      <FlatList
+        data={data?.seeFeed}
+        keyExtractor={(photo) => "" + photo.id}
+        renderItem={renderPhoto}
+      />
+    </ScreenLayout>
   );
 }
