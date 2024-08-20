2de9ba71376f11d1ff6817a7bd1951a845408b4a
minsu.lee <minsu.rob@gmail.com>
2024-08-20 23:18:08 +0900

    17.3: 검색 기능활성화

--- screens/Search.js
+++ screens/Search.js
@@ -1,7 +1,31 @@
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { Text, TextInput, View } from "react-native";
+import styled from "styled-components/native";
+import DismissKeyboard from "../components/DismissKeyboard";
 
+const Input = styled.TextInput``;
 export default function Search({ navigation }) {
+  const { setValue, register } = useForm();
+  const SearchBox = () => (
+    <TextInput
+      style={{ backgroundColor: "white" }}
+      returnKeyLabel="Search"
+      returnKeyType="search"
+      autoCorrect={false}
+      placeholder="Search Photos"
+      placeholderTextColor="black"
+      onChangeText={(text) => setValue("keyword", text)}
+    />
+  );
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: SearchBox,
+    });
+    register("keyword");
+  }, []);
+
   return (
     <View
       style={{
@@ -11,9 +35,18 @@ export default function Search({ navigation }) {
         justifyContent: "center",
       }}
     >
-      <TouchableOpacity onPress={() => navigation.navigate("Photo")}>
-        <Text style={{ color: "white" }}>photo</Text>
-      </TouchableOpacity>
+      <DismissKeyboard>
+        <View
+          style={{
+            backgroundColor: "black",
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "white" }}>photo</Text>
+        </View>
+      </DismissKeyboard>
     </View>
   );
 }
